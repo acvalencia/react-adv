8876842bc4ad4c4a470b8e40360a45beea7e09ba
Camilo Valencia <acvalencia.contact@gmail.com>
2023-01-18 16:43:10 -0500

    component compount

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -3,41 +3,34 @@ import {
   Route,
   NavLink,
   Routes,
-  Navigate
 } from 'react-router-dom';
-import { routes } from './routes';
+import { ShoppingPage } from '../02-component-patterns/pages/ShoppingPage';
 import logo from '../logo.svg';
-import { Suspense } from 'react';
 
 export const Navigation = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Router>
-        <div className="main-layout">
-          <nav>
-            <img src={ logo } alt="React Logo" />
-            <ul>
-              {
-                routes.map( ({ to, name }) => (
-                  <li key={ to }>
-                    {/* <NavLink to={ to } activeClassName="nav-active" exact>{ name }</NavLink> */}
-                    <NavLink to={ to } >{ name }</NavLink>
-                  </li>
-                ))
-              }
-            </ul>
-          </nav>
+    <Router>
+      <div className="main-layout">
+        <nav>
+          <img src={logo} alt="React Logo" />
+          <ul>
+            <li key={'home'}>
+              <NavLink to='/home' >{'Home'}</NavLink>
+            </li>
+            <li key={'about'}>
+              <NavLink to='/shopping' >{'Shopping'}</NavLink>
+            </li>
+          </ul>
+        </nav>
 
-          {/* A <Switch> looks through its children <Route>s and
+        {/* A <Switch> looks through its children <Route>s and
               renders the first one that matches the current URL. */}
-            <Routes>
-              { routes.map( ({ path, Component }) => (
-                  <Route path={ path } element={ <Component /> } />
-              )) }
-              <Route path="/*" element={ <Navigate to={ routes[0].to } replace /> } />
-            </Routes>
-        </div>
-      </Router>
-    </Suspense>
+        <Routes>
+          <Route path='/home' element={<div>Home</div>} />
+          <Route path='/shopping' element={ <ShoppingPage /> } />
+          {/* <Route path="/*" element={ <Navigate to={ routes[0].to } replace /> } /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
