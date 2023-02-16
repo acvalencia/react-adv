046f07f6070d640a3851dc3753536ca2d2846a51
Camilo Valencia <acvalencia.contact@gmail.com>
2023-02-15 19:24:34 -0500

    component extend styles - ready

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,12 +1,19 @@
 import { createContext } from 'react';
 import { useProducts } from '../hooks/useProducts'
-import { ProductCardProps, ProductContextProps } from '../interfaces/interfaces';
+import { Product, ProductContextProps } from '../interfaces/interfaces';
 import styles from '../styles/styles.module.css'
 
 export const ProductContext = createContext({} as ProductContextProps)
 const { Provider } = ProductContext
 
-export const ProductCard = ({ children, product }: ProductCardProps ) => {
+export interface Props {
+  product: Product;
+  children?: React.ReactElement | React.ReactElement[];
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+export const ProductCard = ({ children, product, className, style }: Props ) => {
 
   const { counter, increaseBy } = useProducts()
 
@@ -16,7 +23,10 @@ export const ProductCard = ({ children, product }: ProductCardProps ) => {
       increaseBy,
       product
     }}>
-      <div className={ styles.productCard }>
+      <div
+        style={ style }
+        className={ `${ styles.productCard } ${ className }` }
+      >
         { children }
       </div>
     </Provider>
--- src/02-component-patterns/components/ProductTitle.tsx
+++ src/02-component-patterns/components/ProductTitle.tsx
@@ -2,12 +2,18 @@ import { useContext } from "react"
 import styles from '../styles/styles.module.css'
 import { ProductContext } from "./ProductCard"
 
-export const ProductTitle = ({ title } : { title?: string } ) => {
+export interface Props {
+  className?: string;
+  title?: string;
+  style?: React.CSSProperties;
+}
+
+export const ProductTitle = ({ title, className, style } : Props ) => {
 
   const { product } = useContext( ProductContext )
 
   return (
-    <span className={ styles.productDescription }>
+    <span style={ style } className={ `${ styles.productDescription } ${ className }` }>
       { title ? title : product.title }
     </span>
   )
